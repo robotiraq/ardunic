94715e9e1bae0f2f716e0c7c35a0f70ccc2cd269
Ibrahim Ismail <ibrahim1998i@yahoo.com>
2023-02-15 23:32:52 +0300

    searchMatch

---
+++ src/components/SearchMatch.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="text-center">
+    <img src="./assets/logo.png" />
+    <div class="">
+      Search: <input type="text" v-model="query" placeholder="Search" />
+    </div>
+    <br />
+    <div v-html="highlight()"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      query: "",
+      content: "check if search matches",
+    };
+  },
+  methods: {
+    highlight() {
+      if (!this.query) {
+        return this.content;
+      }
+      return this.content.replace(new RegExp(this.query, "i"), (match) => {
+        return "<strong>" + match + "</strong>";
+      });
+    },
+  },
+};
+</script>
